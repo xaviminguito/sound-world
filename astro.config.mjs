06951a43d9b28792e5fce2ef7ffe50e57257ebd8
Xavi Minguito <xavi.minguito@wam.global>
2024-08-24 23:36:32 +0200

    Customize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
-import vercel from '@astrojs/vercel/serverless';
+
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare(),
 })
