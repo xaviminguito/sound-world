76d851aff4061500d40edb4bbddbba05fd11a2a3
Xavi Minguito <xavi.minguito@wam.global>
2024-08-25 02:17:35 +0200

    Use Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
 
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
