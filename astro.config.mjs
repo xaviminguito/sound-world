c5eaa4df482fbf9981ae2bfe58b410035f656397
Xavi Minguito <xavi.minguito@wam.global>
2024-08-25 02:13:29 +0200

    Use Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
 
-import cloudflare from '@astrojs/cloudflare'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'server',
-  adapter: cloudflare(),
+  adapter: vercel(),
 })
