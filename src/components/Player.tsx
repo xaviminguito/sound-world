829da4d5daea7a8a79ce95e1db99bafdeb2aacc3
Xavi Minguito <xavi.minguito@wam.global>
2024-08-26 23:55:23 +0200

    Delete default src

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -2,44 +2,6 @@ import { currentTrack, isPlaying } from './state'
 
 import { useEffect, useState, useRef } from 'preact/hooks'
 
-const PlayIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    class="w-10 h-10 sm:w-14 sm:h-14"
-    aria-hidden="true"
-    focusable="false"
-  >
-    <path
-      fill-rule="evenodd"
-      d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
-      clip-rule="evenodd"
-    />
-  </svg>
-)
-
-const PauseIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    class="w-10 h-10 sm:w-14 sm:h-14"
-    aria-hidden="true"
-    focusable="false"
-  >
-    <path
-      fill-rule="evenodd"
-      d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z"
-      clip-rule="evenodd"
-    />
-  </svg>
-)
-
-// This app doesn't have real songs, it only has a few songs
-// that we will play over and over as the user uses the app.
-//const MAX_SONGS = 4
-
 export default function Player() {
   const audioPlayer = useRef<HTMLAudioElement>(null)
   const progressRef = useRef(null)
@@ -148,17 +110,7 @@ export default function Player() {
             {artist}
           </div>
         </div>
-        <audio class="min-w-96" ref={audioPlayer} src={currentTrack.value.track} controls />
-        <div class="flex gap-6 items-center text-black">
-          {/*<button
-            type="button"
-            class="focus-visible:ring-2 focus:outline-none focus:ring-black"
-            onClick={() => (isPlaying.value = !isPlaying.value)}
-          >
-            {isPlaying.value ? PauseIcon : PlayIcon}
-            <span class="sr-only">{isPlaying.value ? 'Pause' : 'Play'}</span>
-          </button>*/}
-        </div>
+        <audio class="min-w-96" ref={audioPlayer} controls />
       </div>
     </div>
   )
