a50c0fb912e346dbdf7446c91ce8b71069034b36
Xavi Minguito <xavi.minguito@wam.global>
2024-08-26 21:34:57 +0200

    Login & absolute paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astro.vercel.app',
   integrations: [tailwind(), preact()],
   output: 'server',
   adapter: vercel(),
